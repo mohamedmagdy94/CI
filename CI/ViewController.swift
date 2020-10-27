ed39a981f92d41c3126e540e7ebfc4fdfb8d3cc6
Mohamed ElTaweel <mohamedmagdy.9080@gmail.com>
2020-10-27 17:28:02 +0200

    Initial Commit

---
+++ CI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CI
+//
+//  Created by Mohamed El-Taweel on 10/27/20.
+//  Copyright © 2020 Learning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

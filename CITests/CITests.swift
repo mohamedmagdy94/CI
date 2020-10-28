d924030f59478ed1740fd336aec63370d2ad96ce
Mohamed ElTaweel <mohamedmagdy.9080@gmail.com>
2020-10-28 14:06:09 +0200

    First Test Case

--- CITests/CITests.swift
+++ CITests/CITests.swift
@@ -22,13 +22,15 @@ class CITests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let i = 0
+        XCTAssert(i == 0, "I supposed to be 0")
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    func testPerformanceExample() throws {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 
 }
